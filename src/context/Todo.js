0a43f5f473d5971e2e772919c453d19e68221fd5
onkar kotule <onkar.kotule@bitcs.in>
2023-02-13 18:12:51 +0530

    added dummy data

--- src/context/Todo.js
+++ src/context/Todo.js
@@ -4,8 +4,42 @@ export const TodoContext = createContext(null);
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
+    let dummy = [
+        {
+            "key": 59,
+            "createdAt": "13/02/2023",
+            "title": "dummy 1",
+            "description": "dummy todo",
+            "dueDate": "24/02/2023",
+            "tags": [
+                "f1"
+            ],
+            "status": "open"
+        },
+        {
+            "key": 31,
+            "createdAt": "14/02/2023",
+            "title": "dummy2",
+            "description": "everything working fine",
+            "dueDate": "24/02/2023",
+            "tags": [
+                "f2"
+            ],
+            "status": "Working"
+        },
+        {
+            "key": 14,
+            "createdAt": "13/02/2023",
+            "title": "dummy3",
+            "description": "dg",
+            "dueDate": "23/02/2023",
+            "status": "Working"
+        }
+
+    ]
     let todoObj
     const [Data, setData] = useState([todoObj]);
+    if (Data.length === 0) localStorage.setItem("notes", JSON.stringify(dummy))
     const [Width, setWidth] = useState(window.innerWidth);
     const [FilterData, setFilterData] = useState(Data);
     const [edit, setedit] = useState(false);
